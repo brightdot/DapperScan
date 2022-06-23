fbe5440139672f037926b008ae4a856a58e7124d
Karthikkumar Subramaniam <karthikkumar.subramaniam@likewize.com>
2022-06-23 13:00:35 +0530

    removing params

--- DataLayer.cs
+++ DataLayer.cs
@@ -7,11 +7,7 @@ namespace DapperAlternatives
         public PingResult Ping(string filter)
         {
             var sql = $"SELECT 1 AS [Result] Where {filter}";
-            var parameters = new
-            {
-                PhoneNumber = "12345678"
-            };
-            return DapperExtensions.GetFirstOrDefault<PingResult>(sql, parameters);
+            return DapperExtensions.GetFirstOrDefault<PingResult>(sql);
         }
 
     }
