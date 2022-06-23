923e85afc6ecabdafb7d1fb049bca7fba99e880b
Karthikkumar Subramaniam <karthikkumar.subramaniam@likewize.com>
2022-06-23 13:13:08 +0530

    adding ado.net code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 using Newtonsoft.Json;
 
 namespace DapperAlternatives
 {
     internal class Program
     {
+        public static readonly string EXASCALE_CONNECTIONSTRING = ConfigurationManager.ConnectionStrings["ExascaleDB"].ConnectionString;
         static void Main(string[] args)
         {
             var dataLayer = new DataLayer();
@@ -13,7 +16,24 @@ namespace DapperAlternatives
 
             var authDetail = dataLayer.Ping("'1' = '1'");
 
+            string filter = "'1'='1'";
+            var sql = $"SELECT 1 AS [Result] Where {filter}";
+
             Console.WriteLine(JsonConvert.SerializeObject(authDetail));
+
+
+            using (var connection = new SqlConnection(EXASCALE_CONNECTIONSTRING))
+            {
+                connection.Open();
+
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = sql;
+
+                int rows = cmd.ExecuteNonQuery();
+
+                connection.Close();
+                Console.WriteLine("No of rows affected:" + rows);
+            }
 
             Console.ReadLine();
 
